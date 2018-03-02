84a44f7f5862ab7414201572ea6870ff8d6d3e4f
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2018-03-02 10:49:42 +0000

    Update and rename cel.c to kthpos.c

--- cel.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int v;
-	scanf("%d",&v);
-	printf("%d",v+273);
-	return 0;
-}
---
+++ kthpos.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int v,k,a[10],i;
+	scanf("%d%d",&v,&k);
+	for(i=1;i<=v;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	printf("%d",a[k]);
+	return 0;
+	}
